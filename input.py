80c7599679f7ce44c1a8370ff2d5bcc6efb0202c
gucci1909 <umangarora0134@gmail.com>
2024-12-12 01:53:50 +0530

    python course

---
+++ input.py
@@ -0,0 +1,12 @@
+# input always comes as string
+a = input()
+print("the a value is", type(a))
+b = input()
+print("the b value is", int(b)+int(a))
+# triple inverted coma's also we can write here
+c = '''Hello
+hi bro
+hey bro,
+"Hello sir"
+'''
+print(c);
