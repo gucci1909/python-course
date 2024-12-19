81f27899864d85b7ac6078eacd357e945dda013a
gucci1909 <umangarora0134@gmail.com>
2024-12-20 02:53:09 +0530

    python-course

---
+++ set.py
@@ -0,0 +1,14 @@
+s = {2, 4, 2, 6}
+# they are not in order when print
+print(s)
+# set doesn't have key's 
+
+info = {"Carla", 19, False, 5.9, 19}
+print(type(info))
+
+# this is how we make empty set
+harry = set()
+print(type(harry))
+
+for value in info:
+  print(value)
